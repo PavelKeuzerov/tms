223c5d4d630d4e00fb0ad4dcc5fa501222a16e1d
pavel <pit23111987@gmail.com>
2022-12-10 14:38:34 +0300

    my_first_PR

--- les1.rb
+++ les1.rb
@@ -1,4 +1,8 @@
-it is my first commit 
-# добавить фамилию имя в test
-павел кейзеров
-"переделать"
+# "it is my first commit 
+# # добавить фамилию имя в test
+# павел кейзеров
+# "переделать""
+
+Павел Кейзеров 
+
+it is my first commit
