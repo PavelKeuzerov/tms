15606277367ecdb692f0c33b3b0fd7fe78b61d34
pavel <pit23111987@gmail.com>
2022-12-14 12:09:24 +0300

    task_2 task_4

---
+++ lesson2/task2.rb
@@ -0,0 +1,12 @@
+# запрос ввода данных 
+# вывод результата 
+
+puts 'введите ваше имя'
+name = gets.chomp
+puts 'введите ваш рост'
+growth = gets.to_f
+weight = (growth - 110)*1.15
+puts "#{name}, ваш идеальный вес состовляет #{weight}"
+if weight < growth
+  puts 'ваш вес оптимальный'
+end
