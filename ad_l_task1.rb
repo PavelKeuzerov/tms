506c53114b55e211b165bab5af94aeb845388904
pavel <pit23111987@gmail.com>
2022-12-17 15:27:31 +0300

    home_worck2

--- ad_l_task1.rb
+++ ad_l_task1.rb
@@ -4,6 +4,13 @@
 #   Если D < 0, то выводим дискриминант и сообщение "Корней нет"
 # Подсказка: Алгоритм решения с блок-схемой (www.bolshoyvopros.ru). Для вычисления квадратного корня, нужно использовать  
 
+# 1 ввод данных пользователем
+# 2 вычисление дискриминанта и корней уравнения
+
+
+
+
+
 
 puts 'Ведите коэфициент a'
 a = gets.to_f
@@ -11,13 +18,13 @@ puts 'Ведите коэфициент b'
 b = gets.to_f
 puts 'Ведите коэфициент c'
 c = gets.to_f
-discriminant = b**2 - 4*a*c           
+discriminant = b**2-4*a*c           
 if discriminant < 0 
   puts "#{discriminant} Корней нет"
   elsif discriminant == 0 
-    puts "#{discriminant} он имеет один корень #{-b/(a*2)}"   # D = -b/2a
+    puts "#{discriminant} он имеет один корень #{-b/(a*2)}"   
   else discriminant > 0
-    puts "#{discriminant} он имеет два корня #{( -b - Math.sqrt(discriminant))/(a * 2)} и #{( -b + Math.sqrt(discriminant))/(a * 2)}"   #Math.sqrt
+    puts "#{discriminant} он имеет два корня #{(-b-Math.sqrt(discriminant))/(a*2)} и #{(-b+Math.sqrt(discriminant))/(a*2)}"   
 end      
 
 
