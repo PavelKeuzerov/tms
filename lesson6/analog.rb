7b09786e8a08f2a3289d16bb2feebdfc61628838
Pavel <pit23111987@gmail.com>
2023-01-16 00:06:32 +0300

    h/w_6

--- lesson6/analog.rb
+++ lesson6/analog.rb
@@ -1,6 +1,6 @@
 # Создать свой аналог класса Array, String, Hash (на выбор).
 
-class Analog
+class String
   attr_accessor :numbers
 
   def initialize(numbers)
@@ -8,14 +8,19 @@ class Analog
   end
 
   def size
-    numbers.size
+    count = 0
+    numbers.each_char { |char| count += 1 }
+    count
   end
 
-  # def years_left 
-  #   years_left = 2
-  #   numbers * 2
-  # end
+  def delete(del)
+    new_str = ''
+    numbers.each_char { |char| new_str += char unless char == del.to_s }
+    new_str
+  end
 end
-ar = Analog.new('23543')
-# ar.years_left
-ar.size
+
+str = String.new('23543')
+
+p str.size
+p str.delete(3)
--- lesson6/soda.rb
+++ lesson6/soda.rb
@@ -4,16 +4,16 @@
 
 # 1-Создать class принимающий аргумент
 # 2-Воспользоваться методом show my drink с выводом на экран фраз
-# require 'pry'
+
 class Soda
   attr_reader :addition
 
-  def initialize(addition = nil)
+  def initialize(addition = '')
     @addition = addition
   end
 
   def show_my_drink
-    if addition.nil?
+    if addition == ''
       puts 'Обычная газировка'
     else
       puts "Ваша выбор газировка с #{addition}"
@@ -23,5 +23,3 @@ end
 
 carbonated = Soda.new
 carbonated.show_my_drink
-
-# binding.pry
