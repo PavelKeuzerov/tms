37757bd3593c45902c864feca1e3411f2df9600e
Pavel <pit23111987@gmail.com>
2023-01-15 11:29:45 +0300

    15.01

--- lesson6/analog.rb
+++ lesson6/analog.rb
@@ -7,14 +7,15 @@ class Analog
     @numbers = numbers
   end
 
-  # def size(size)
-  #   numbers.size
-  # end
-
-  def years_left 
-    years_left = 2
-    numbers *2
+  def size
+    numbers.size
   end
+
+  # def years_left 
+  #   years_left = 2
+  #   numbers * 2
+  # end
 end
-ar = Analog.new(23543)
-ar.years_left
+ar = Analog.new('23543')
+# ar.years_left
+ar.size
--- lesson6/soda.rb
+++ lesson6/soda.rb
@@ -4,21 +4,24 @@
 
 # 1-Создать class принимающий аргумент
 # 2-Воспользоваться методом show my drink с выводом на экран фраз
+# require 'pry'
 class Soda
   attr_reader :addition
 
-  def initialize(addition)
+  def initialize(addition = nil)
     @addition = addition
   end
 
   def show_my_drink
-    if addition
-      puts "Ваша выбор газировка с #{addition}"
-    else
+    if addition.nil?
       puts 'Обычная газировка'
+    else
+      puts "Ваша выбор газировка с #{addition}"
     end
   end
 end
 
-carbonated = Soda.new('cherry_syrup')
+carbonated = Soda.new
 carbonated.show_my_drink
+
+# binding.pry
