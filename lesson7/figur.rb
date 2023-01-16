321587fb5418c20bffe730549d5aeff7557f686b
Pavel <pit23111987@gmail.com>
2023-01-16 17:53:39 +0300

    h/w_7 1t,2t,4t

--- lesson7/figur.rb
+++ lesson7/figur.rb
@@ -1,4 +1,4 @@
-# Реализуйте иерархию классов Figure, Circle, Square, Rectangle, Triangle с методами area() и perimeter(),
+#Реализуйте иерархию классов Figure, Circle, Square, Rectangle, Triangle с методами area() и perimeter(),
 # которые возвращают площадь и периметр фигуры. Используйте полиморфизм. 
 
 class Figure
