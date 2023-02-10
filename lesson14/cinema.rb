faa83301ec9f3eb935bb4d1d4939b7daf4086b14
Pavel Keuzerov <pit23111987@gmail.com>
2023-02-10 21:57:46 +0300

    10/12

--- lesson14/cinema.rb
+++ lesson14/cinema.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 
 # Разработка автоматизированной системы работы кинотеатра
 #
@@ -38,59 +37,66 @@ class Movie
     @name = name
     @timing = timing
   end
+
+  def movies
+    puts "genre: #{genre}, name: #{name}, timing: #{timing}"
+  end
 end
 
 class Session
   attr_accessor :type, :price, :films
 
-  def initialize(type, price)
+  def initialize(type, price, films = [])
     @type = type
     @price = price
-    @films = {}
+    @films = films
   end
 
   def add_film(new_film)
-    films[new_film] = { genre: new_film.genre, name: new_film.name, timing: new_film.timing }
-  end
-
-  def info
-    films.values.join(', ')
+    films << new_film
   end
 
   def info_session
-    puts "#{type}, price #{price}, movie #{info}"
+    puts "#{type}, price #{price}"
+    films.map(&:movies)
   end
+
+  # def genre(genre)
+  #   films.select { |films| films.genre == genre }
+  # end
+
+  # def selector(&block)
+  #   films.select(&block)
+  # end
 end
 
 class Tickets
   attr_accessor :row, :place, :tickets
 
-  def initialize(row, place)
+  def initialize(row, place, tickets = [])
     @row = row
     @place = place
-    @tickets = {}
+    @tickets = tickets
   end
 
   def add_ticket(new_ticket)
-    tickets[new_ticket] = { type: new_ticket.type, price: new_ticket.price }
-  end
-
-  def info
-    tickets.values.join(', ')
+    tickets << new_ticket
   end
 
   def info_ticket
-    puts " row - #{row}, place - #{place}, #{info}"
+    puts " row - #{row}, place - #{place}"
+    tickets.map(&:info_session)
   end
 end
 
 class User
-  attr_accessor :user_name, :password, :purchased_ticket
+  attr_accessor :user_name, :password, :purchased_ticket, :user_email
 
-  def initialize(user_name, password)
+  def initialize(user_name, password, user_email, purchased_ticket = [])
     @user_name = user_name
     @password = password
-    @purchased_ticket = {}
+    @user_email = user_email
+    @purchased_ticket = purchased_ticket
   end
 
   def user_verification
@@ -98,45 +104,53 @@ class User
     user_name = gets.strip
     puts 'Enter a password containing letters or numbers (5 characters)'
     password = gets.strip
-    raise 'Некоректный ввод' unless user_name.match(/^[a-zA-Z]{1,10}$/) && password.match(/^[a-zA-Z0-9]{1,5}$/)
+    raise 'Incorrect data entry' unless user_name.match(/^[a-zA-Z]{1,10}$/) && password.match(/^[a-zA-Z0-9]{1,5}$/)
 
     puts "Welcome #{user_name}"
   end
 
   def buy_ticket(buy_ticket)
-    purchased_ticket[buy_ticket] = buy_ticket
-    # purchased_ticket[buy_ticket] =
-    #   { type: buy_ticket.type, price: buy_ticket.price, genre: buy_ticket.genre, name: buy_ticket.name,
-    #     timing: buy_ticket.timing }
-  end
-
-  def info
-    purchased_ticket.values.join(', ')
+    purchased_ticket << buy_ticket
   end
 
   def buy_info
-    puts "Вы купили #{purchased_ticket}"
+    puts 'Your ticket'
+    purchased_ticket.map(&:info_ticket)
+  end
+
+  def send_email
+    puts 'Enter your email and we will send you a copy of your ticket'
+    user_email = gets.strip
+    raise 'Incorrect data entry' unless user_email.match(/^[a-z0-9]+@[a-z0-9]+\.[a-z]+/)
+
+    puts 'Letter sent'
   end
 end
 
 terminator = Movie.new('action', 'Terminator', '2:05')
 shrek = Movie.new('cartoon', 'Shrek', '1:40')
 
-evening = Session.new('Evening session', '5')
-afternoon = Session.new('Afternoon session', '3')
+evening = Session.new('Evening session', '5', [terminator])
+afternoon = Session.new('Afternoon session', '3', [shrek])
 # afternoon.add_film(shrek)
 # evening.add_film(terminator)
 # evening.info_session
 # afternoon.info_session
+# evening.genre
 
-one = Tickets.new('2', '3')
-two = Tickets.new('4', '5')
+one = Tickets.new('2', '3', [evening])
+two = Tickets.new('4', '5', [afternoon])
 # one.add_ticket(evening)
-two.add_ticket(afternoon)
+# two.add_ticket(afternoon)
 # one.info_ticket
-two.info_ticket
+# two.info_ticket
 
-ar = User.new('', '')
-ar.buy_ticket(two)
-ar.buy_info
-# ar.user_verification
+ar = User.new('', '', '', [two])
+# ar.buy_ticket(two)
+
+
+ar.user_verification
+# ar.buy_info
+# ar.send_email
+
+
