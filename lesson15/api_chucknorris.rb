a2c9ede7718b2f012532fe8665318cddd1c5090a
Pavel Keuzerov <pit23111987@gmail.com>
2023-02-16 23:57:40 +0300

    chuck random_categotys

--- lesson15/api_chucknorris.rb
+++ lesson15/api_chucknorris.rb
@@ -3,23 +3,6 @@
 require 'curb'
 require 'json'
 
-# random_category = Curl.get('https://api.chucknorris.io/jokes/random')
-# chuck = JSON random_category.body
-# # puts chuck
-# puts chuck['value']
-
-# res = Curl.get('https://api.chucknorris.io/jokes/random?category={category}')
-# chuck = JSON parse(res.body)
-# puts chuck
-
-# re = Curl.get('https://api.chucknorris.io/jokes/categories')
-# chuck = JSON re.body
-# puts chuck
-
-# puts res.code
-# puts res.head
-#
-
 class Chucky
   def speak_chuk
     random_category = Curl.get('https://api.chucknorris.io/jokes/random')
