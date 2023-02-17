4ff88f20e95361746db652e5d3693be4eed55864
Pavel Keuzerov <pit23111987@gmail.com>
2023-02-17 22:19:54 +0300

    fix verificat

--- lesson10/verification.rb
+++ lesson10/verification.rb
@@ -9,69 +9,34 @@
 # WrongPasswordException и WrongLoginException - пользовательские классы исключения с двумя конструкторами – один по умолчанию,
 # второй принимает сообщение исключения и передает его в конструктор класса Exception.
 # Обработка исключений проводится внутри метода.
-# Метод возвращает true, если значения верны или false в другом случае.
-
-class WrongLoginException < StandardError
-  attr_accessor :login
-
-  def initialize(login)
-    @login = login
-     super
-  end
-
-  def login_input
-    raise WrongLoginException unless login != /^[a-zA-Z0-9_]{1,19}$/
-
-    true
-  rescue WrongLoginException => e
-    puts e.message
-
-    false
-  end
-end
+# # Метод возвращает true, если значения верны или false в другом случае.
 
 class WrongPasswordException < StandardError
-  attr_accessor :password, :confirm_password
-
-  def initialize(password, confirm_password)
-    @password = password
-    @confirm_password = confirm_password
-    # super
-  end
-
-  def password_input
-    raise WrongPasswordException unless password == /^[a-zA-Z0-9_]{1,19}$/
-
-    true
-  rescue WrongPasswordException => e
-    puts e.message
-
-    false
-  end
-
-  def confirm_password(_pasword_input)
-    raise WrongPasswordException unless password != confirm_password
-  end
-
-  # def pasword_input
-  #   password == /^[a-zA-Z][a-zA-Z0-9]_\.{1,20}$/
-  # rescue WrongPasswordException => e
-  #   puts "An error has occurred #{e.class.name}"
-  #   puts e.message
-  # end
-
-  # def confirm_password(_pasword_input)
-  #   confirm_password == password
-  # rescue WrongPasswordException => e
-  #   puts "An error has occurred #{e.class.name}"
-  #   puts e.message
-  # rescue StandardError
-  #   puts 'confirm_password does not match passworrd'
-  # end
 end
 
-a = WrongLoginException.new('s&&&')
-b = WrongPasswordException.new('afaf_2121', 'asassa12_21')
-# p a.login_input
-# p b.password_input
-# p b.confirm_password
+class WrongLoginException < StandardError
+end
+
+class Verification
+  attr_accessor :login, :password, :confirm_password
+
+  def initialize(login, password, confirm_password)
+    @login = login
+    @password = password
+    @confirm_password = confirm_password
+  end
+
+  def verification
+    raise WrongLoginException unless login.match(/^[a-zA-Z0-9_]{1,20}$/)
+    raise WrongPasswordException unless password.match((/^[a-zA-Z0-9_]{1,20}$/)) && password == confirm_password
+
+    true
+  rescue WrongLoginException, WrongPasswordException => e
+    puts e.message
+    false
+  end
+end
+
+user = Verification.new('12sder_wefrgtylgfef3', '12sder_wefrgtylgfef3', '12sder_wefrgtylgfef3')
+user.verification
+
