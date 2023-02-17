4e7c53571440bf8353d91b67dddea7c6c0659c2b
Pavel Keuzerov <pit23111987@gmail.com>
2023-02-17 13:39:34 +0300

    new metta

--- lesson13/metaprograming.rb
+++ lesson13/metaprograming.rb
@@ -3,31 +3,23 @@
 # Воспользуйтесь возможностями метапрограммирования и добавьте его в свои предыдущие работы.
 # (Синглтон-методы, синглтон-классы, define_method, send, instance_eval, class_eval)
 
-class Dog
-  attr_reader :age, :name
-
-  def initialize(age, name)
-    @age = age
-    @name = name
-  end
-
-  def speak_name
-    puts "My name #{name}"
-  end
-
-  def method_missing(method_name, *_args)
-    if method_name == :speak_age
-      puts "My age #{age}"
-    else
-      super
-      puts "NO method #{method_name}"
+module MyAccer
+  def my_attr(*variable)
+    variable.each do |variable|
+      define_method(variable) do
+        instance_variable_get("@#{variable}")
+      end
     end
-  end
-
-  def respond_to_missing?(method, *_args)
-    method == method_name || super
   end
 end
 
-dog = Dog.new('1', 'Nikodim')
-dog.speak_age
+class MyAttrAccer
+  extend MyAccer
+
+  my_attr :arg_1, :arg_2
+
+  def initialize(arg_1, arg_2)
+    @arg_1 = arg_1
+    @arg_2 = arg_2
+  end
+end
