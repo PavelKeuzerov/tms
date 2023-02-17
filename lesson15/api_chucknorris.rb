b2d3bb122863ce025e90b29f74a361a4ca51c266
Pavel Keuzerov <pit23111987@gmail.com>
2023-02-18 02:16:39 +0300

    chuck

--- lesson15/api_chucknorris.rb
+++ lesson15/api_chucknorris.rb
@@ -4,11 +4,23 @@ require 'curb'
 require 'json'
 
 class Chucky
+  NUMBER_CATEGOIES = 15
+
   def speak_chuk
-    random_category = Curl.get('https://api.chucknorris.io/jokes/random')
-    chuck = JSON random_category.body
-    puts chuck['value']
+    category = Curl.get('https://api.chucknorris.io/jokes/categories')
+    chuck = JSON category.body
+    puts 'Choose a category for the joke from 1 to 16'
+    chuck.map.with_index do |element, index|
+      puts "#{index + 1}: #{element}"
+    end
+    category_selection = gets.strip.to_i - 1
+    random_category = chuck[category_selection]
+    chuck_category = Curl.get("https://api.chucknorris.io/jokes/random?category=#{random_category}")
+    raise 'ERROR - wrong category selection' if category_selection > NUMBER_CATEGOIES
+
+    puts JSON.parse(chuck_category.body)['value']
   end
 end
+
 chuck = Chucky.new
 chuck.speak_chuk
